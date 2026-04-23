85bd8635b0b9cb9a6cf1a510b3284aabbb6381a1
Alejandro24H <oacostaag@unbosque.edu.co>
2026-04-23 21:48:24 +0000

    feat: agregar app.js y tests iniciales

---
+++ app.js
@@ -0,0 +1,9 @@
+function sumar(a, b) {
+  return a + b;
+}
+
+function hola() {
+  return "Hola Mundo";
+}
+
+module.exports = { sumar, hola };
---
+++ app.test.js
@@ -0,0 +1,9 @@
+const { sumar, hola } = require('./app');
+
+test('suma 2 + 3 y debe dar 5', () => {
+  expect(sumar(2, 3)).toBe(5);
+});
+
+test('la función hola devuelve el mensaje correcto', () => {
+  expect(hola()).toBe("Hola Mundo");
+});
